a11feaea32ab0f79803637532eb51ab1ba38fb4d
Stephen Vector <55390907+stephenvector@users.noreply.github.com>
2021-03-11 17:12:55 -0800

    update button styling

--- src/Button.tsx
+++ src/Button.tsx
@@ -2,11 +2,16 @@ import styled from "styled-components";
 
 const Button = styled.button`
   font-family: inherit;
-  font-size: 0.9rem;
-  line-height: 0.9rem;
-  padding: 0.5rem;
+  font-size: 1rem;
+  line-height: 1;
+  padding: 0.75rem 1rem;
   display: block;
   cursor: pointer;
+  background: #f2f2f2;
+  border: 1px solid #ddd;
+  border-radius: 0.25rem;
+  font-weight: 700;
+  color: #000;
 `;
 
 export default Button;
