d2be0d1352206ebdf06d84da6c512f3143088960
Stephen Vector <55390907+stephenvector@users.noreply.github.com>
2021-03-11 17:35:43 -0800

    styling communities page

--- src/Communities.tsx
+++ src/Communities.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import useCommunities from "./useCommunities";
+import Box from "./Box";
+import Container from "./Container";
 
 const Communities: React.FC = () => {
   const { communities } = useCommunities();
   return (
-    <div>
+    <Container>
       <h1>Communities</h1>
       <Link to="/communities/new">New Community</Link>
 
       {Object.entries(communities).map(([communityId, community]) => (
-        <div key={communityId}>
+        <Box key={communityId}>
           <Link to={`/communities/${communityId}`}>
             <h3>{community.name}</h3>
-            <p>{community.description}</p>
+            <div>{community.description}</div>
           </Link>
-        </div>
+        </Box>
       ))}
-    </div>
+    </Container>
   );
 };
 
