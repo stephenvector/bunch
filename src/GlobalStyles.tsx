f05714bdcb00bab0f0482af30cf0166bc0163cd1
Stephen Vector <55390907+stephenvector@users.noreply.github.com>
2021-03-11 17:16:00 -0800

    adjusting global heading margins, setting default font

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -10,7 +10,17 @@ const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    font-family: sans-serif;
+    font-family: 'Nunito', sans-serif;
+    background: #f2f2f2;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin-top: 0;
   }
 `;
 
