2a16422d80db95cfd0d05f9f97b00f609e7f4bca
Stephen Vector <55390907+stephenvector@users.noreply.github.com>
2021-03-11 17:37:41 -0800

    adding post page

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import AppHeader from "./AppHeader";
 import Home from "./Home";
 import Communities from "./Communities";
 import NewCommunity from "./NewCommunity";
-import SignUp from "./SignUp";
+import SignUpPage from "./SignUpPage";
 import Community from "./Community";
 import NewPost from "./NewPost";
+import Post from "./Post";
 
 function App() {
   return (
@@ -15,14 +16,19 @@ function App() {
       <section>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/signup" exact component={SignUp} />
+          <Route path="/signup" exact component={SignUpPage} />
           <Route path="/communities" exact component={Communities} />
           <Route path="/communities/new" exact component={NewCommunity} />
           <Route path="/communities/:communityId" exact component={Community} />
           <Route
-            path="/communities/:communityId/post"
+            path="/communities/:communityId/posts/new"
             exact
             component={NewPost}
+          />
+          <Route
+            path="/communities/:communityId/posts/:postId"
+            exact
+            component={Post}
           />
         </Switch>
       </section>
