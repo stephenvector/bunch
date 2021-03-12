9530a6e6b8bbd5b0d2f9b9b8c9a080a126c1a8b4
Stephen Vector <55390907+stephenvector@users.noreply.github.com>
2021-03-11 17:38:52 -0800

    getting post page to have comments

--- src/Post.tsx
+++ src/Post.tsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import firebase from "firebase/app";
 import "firebase/firestore";
-import { Post, FetchStatus } from "./types";
+import { Post as PostType, FetchStatus } from "./types";
 import useCommunity from "./useCommunity";
+import PostCommentForm from "./PostCommentForm";
+import PostCommentsList from "./PostCommentsList";
+import Loading from "./Loading";
+import Container from "./Container";
+import PageTitle from "./PageTitle";
 
 const Post: React.FC = () => {
   const { communityId, postId } = useParams<{
     communityId: string;
     postId: string;
   }>();
-  const [post, setPost] = useState<Post | null>(null);
+  const [post, setPost] = useState<PostType | null>(null);
   const [postStatus, setPostStatus] = useState<FetchStatus>("initial");
   const { community, status: communityStatus } = useCommunity(communityId);
 
@@ -21,17 +26,59 @@ const Post: React.FC = () => {
       .collection("posts")
       .doc(postId)
       .get()
-      .then((docSnapshot) => {
-        const docData = docSnapshot.data();
-      });
+      .then(
+        (docSnapshot) => {
+          const docData = docSnapshot.data();
+          console.log(docData);
+          if (
+            docData &&
+            "title" in docData &&
+            "content" in docData &&
+            "userId" in docData
+          ) {
+            setPost({
+              title: docData.title,
+              content: docData.content,
+              communityId: docData.communityId,
+              userId: docData.userId,
+              date: docData.date.toDate(),
+            });
+            setPostStatus("success");
+          }
+        },
+        () => {
+          setPostStatus("error");
+        }
+      );
   }, [communityId, postId]);
 
-  if (post === null) return null;
+  if (
+    postStatus === "loading" ||
+    postStatus === "initial" ||
+    communityStatus === "initial" ||
+    communityStatus === "loading"
+  ) {
+    return <Loading />;
+  }
+
+  if (post === null || community === null) return null;
 
   return (
-    <div>
-      <h1>{post.title}</h1>
+    <Container>
+      <PageTitle>{post.title}</PageTitle>
+      <div>
+        Posted in{" "}
+        <Link to={`/communities/${communityId}`}>{community.name}</Link>
+      </div>
       <p>{post.content}</p>
-    </div>
+
+      <h3>Comments</h3>
+
+      <PostCommentForm postId={postId} communityId={communityId} />
+
+      <PostCommentsList postId={postId} />
+    </Container>
   );
 };
+
+export default Post;
